75a87da149f1050102154cca3a6c6e9c9b11a7fa
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-26 14:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function scuberGreetingForFeet(someValue) {
   let result;
   if (someValue <= 400) {
     result = 'This one is on me!'
-  } else if (someValue > 2000) {
-    result = 'I will gladly take your thirty bucks.'
   } else if (someValue > 2500) {
     result = 'No can do.'
+  } else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.'
   }
   return result
 }
